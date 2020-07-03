77634a428ca70222b7353c1a0cab49d19de9a97a
Haya <max9173627@gmail.com>
2020-07-03 13:08:43 +0900

    [add]名前と電話番号のフォームを追加

--- app/src/main/java/com/haya/selfintroductionapp/MainActivity.java
+++ app/src/main/java/com/haya/selfintroductionapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.haya.selfintroductionapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button click = findViewById(R.id.click_Button);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Log.i("クリックテスト", "出力確認");
+//                EditText editName = findViewById(R.id.edtName);
+//                EditText edtPhone = findViewById(R.id.edtPhone);
+//                Log.i("クリックテスト", edtPhone.getText().toString() );
+            }
+        });
     }
 }
